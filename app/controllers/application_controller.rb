b1766e2f647e9ede749120ec48d14c8975fc4995
Nedzib Sastoque <nedzib.sastoque@symplifica.com>
2022-05-10 08:16:59 -0500

    login adjustment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   require 'faker'
   before_action(:configure_sign_up_params, if: :devise_controller?)
+  before_action :authenticate_user!
 
   def configure_sign_up_params
     p "entry"
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
   def index
-    redirect_to "/user/sign_in" unless signed_in?
   end
 end
